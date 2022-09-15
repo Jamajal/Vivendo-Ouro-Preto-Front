aea098405fe1bfba1bcebb1219fc1d6b21f98da5
Jamajal <leandroliberiomachado@hotmail.com>
2022-09-14 23:13:46 -0300

    Criando estrutura base

---
+++ front/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header'
+
+import { StyledHome } from './StyledHome'
+
+
+export default function Home(){
+    return(
+        <StyledHome>
+            <Header />
+        </StyledHome>
+    )
+}
